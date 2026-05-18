e4b4435902e62bca1a711a30eeaede17a83c4dc8
yjdyamv <yjdyamv@tutamail.com>
2026-05-18 15:57:42 +0800

    fix(napi): fix stubs_napi.cpp include + lambda issues
    - Remove broken ../ include for RasterBandPixelsNapi
    - Remove lambda-based InstanceMethod (not supported in node-addon-api v8)
    - All 1443 tests pass

--- src/gdal_stubs_napi.cpp
+++ src/gdal_stubs_napi.cpp
@@ -2,7 +2,15 @@
 
 namespace node_gdal {
 
-#define IMPL_STUB(klass) \
+// RasterBandPixelsNapi
+Napi::FunctionReference RasterBandPixelsNapi::constructor;
+Napi::Object RasterBandPixelsNapi::Init(Napi::Env env, Napi::Object exports) {
+  Napi::Function f = DefineClass(env, "RasterBandPixelsNapi", {});
+  constructor = Napi::Persistent(f); constructor.SuppressDestruct();
+  exports.Set("RasterBandPixelsNapi", f); return exports;
+}
+
+#define STUB_TOSTRING(klass) \
   Napi::FunctionReference klass::constructor; \
   Napi::Object klass::Init(Napi::Env env, Napi::Object exports) { \
     Napi::Function f = DefineClass(env, #klass, {}); \
@@ -10,21 +18,20 @@ namespace node_gdal {
     exports.Set(#klass, f); return exports; \
   }
 
-IMPL_STUB(RasterBandPixelsNapi)
-IMPL_STUB(RasterBandOverviewsNapi)
-IMPL_STUB(LayerFieldsNapi)
-IMPL_STUB(FeatureFieldsNapi)
-IMPL_STUB(FeatureDefnFieldsNapi)
-IMPL_STUB(GeometryCollectionChildrenNapi)
-IMPL_STUB(PolygonRingsNapi)
-IMPL_STUB(LineStringPointsNapi)
-IMPL_STUB(CompoundCurveCurvesNapi)
-IMPL_STUB(GroupGroupsNapi)
-IMPL_STUB(GroupArraysNapi)
-IMPL_STUB(GroupAttributesNapi)
-IMPL_STUB(GroupDimensionsNapi)
-IMPL_STUB(ArrayDimensionsNapi)
-IMPL_STUB(ArrayAttributesNapi)
+STUB_TOSTRING(RasterBandOverviewsNapi)
+STUB_TOSTRING(LayerFieldsNapi)
+STUB_TOSTRING(FeatureFieldsNapi)
+STUB_TOSTRING(FeatureDefnFieldsNapi)
+STUB_TOSTRING(GeometryCollectionChildrenNapi)
+STUB_TOSTRING(PolygonRingsNapi)
+STUB_TOSTRING(LineStringPointsNapi)
+STUB_TOSTRING(CompoundCurveCurvesNapi)
+STUB_TOSTRING(GroupGroupsNapi)
+STUB_TOSTRING(GroupArraysNapi)
+STUB_TOSTRING(GroupAttributesNapi)
+STUB_TOSTRING(GroupDimensionsNapi)
+STUB_TOSTRING(ArrayDimensionsNapi)
+STUB_TOSTRING(ArrayAttributesNapi)
 
 namespace VsiNapi {
 Napi::Object Init(Napi::Env env, Napi::Object exports) {
